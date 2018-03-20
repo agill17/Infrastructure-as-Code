13db1382bd126cddcb2e321a9e3e39ca0938b640
Amrit Gill <amgill1234@gmail.com>
2018-03-20 15:17:32 -0400

    clean up

--- Terraform/tomcat_3_az_deployment/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  secret_key = "${var.aws_secret}"
-  access_key = "${var.aws_access}"
-  region = "${var.default["region"]}"
-}
--- Terraform/tomcat_3_az_deployment/vars.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_secret" {}
-variable "aws_access" {}
-
-variable "default" {
-  default = {
-    region = "us-east-1"
-  }
-}
