531669733abffc4023b8cbb2fe8a8ec245abf799
Amrit Gill <amgill1234@gmail.com>
2018-03-27 23:14:44 -0400

    write some ideas on how to configure and deploy

---
+++ Chef/tomcat-jenkins/recipes/ci_job.rb
@@ -0,0 +1,10 @@
+### ci job will run -- build, 
+### unit tests, lints,
+### deploy artifact to s3 or nexus
+### trigger selenium ? -- maybe
+### run deploy job 
+#      -- run tf and make sure infra is ready
+#      -- then run recipes or playbooks on servers
+#         -- config-mng: setup app servers and deploy artifacts to app servers
+
+# run all this as part of pipeline job. yea lets try that out
---
+++ Chef/tomcat-jenkins/recipes/jenkins-setup.rb
@@ -0,0 +1,11 @@
+
+
+### set up credentials
+### set up global-config-tools - mvn, jdk, gradle, and more
+### set up thin back up plugin 
+
+
+jenkins_password_credentials 'git-creds' do
+	id "amrit-git-creds"
+	password ""
+end
