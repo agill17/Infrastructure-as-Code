1373dc26dcf658ee1fa6c42f44d2eafaf530db2a
Amrit Gill <amgill1234@gmail.com>
2018-03-27 23:22:27 -0400

    tf updates

--- Terraform/env_deployments/test/main.tf
+++ Terraform/env_deployments/test/main.tf
@@ -1,1 +1,91 @@
+variable "azs" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+}
 
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "test-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    Name = "test-igw"
+  }
+}
+
+resource "aws_subnet" "public_subnets" {
+  count      = 3
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.${count.index+1}.0/24"
+
+  tags = {
+    Name = "test-public-subent-${count.index+1}"
+  }
+
+  map_public_ip_on_launch = true
+  availability_zone       = "${element(var.azs, count.index)}"
+}
+
+resource "aws_subnet" "private_subnet" {
+  count      = 1
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.${count.index+4}.0/24"
+
+  tags = {
+    Name = "test-private-subent-${count.index+1}"
+  }
+
+  map_public_ip_on_launch = false
+  availability_zone       = "${element(var.azs, count.index)}"
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+}
+
+resource "aws_route_table_association" "rt_subnet_associate" {
+  count          = 3
+  subnet_id      = "${element(aws_subnet.public_subnets.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public_rt.id}"
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  count         = 1
+  allocation_id = "${aws_eip.eip.id}"
+  subnet_id     = "${element(aws_subnet.public_subnets.*.id,count.index)}"
+}
+
+resource "aws_security_group" "sg" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  name   = "test-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- Terraform/env_deployments/test/vars.tf
+++ Terraform/env_deployments/test/vars.tf
@@ -1,1 +1,59 @@
+variable "aws_access" {}
+variable "aws_secret" {}
 
+variable "app" {
+  default = "addressbook"
+}
+
+variable "env" {
+  default = "dev"
+}
+
+variable "public_key" {
+  default = "dev.pub"
+}
+
+variable "private_key" {
+  default = "dev.pem"
+}
+
+variable "key_name" {
+  default = "dev"
+}
+
+variable "ssh_user" {
+  default = "centos"
+}
+
+variable "default" {
+  default = {
+    region        = "us-east-1"
+    az            = "us-east-1d"
+    type          = "t2.medium"
+    root_ebs_type = "gp2"
+    root_ebs_size = "8"
+  }
+}
+
+variable "amis" {
+  default = {
+    us-east-1 = "ami-4bf3d731"
+    us-east-2 = "ami-be96bcdb"
+    us-west-1 = "ami-72aba312"
+  }
+}
+
+variable "ssh" {
+  default = {
+    private_key = "dev.pem"
+    key_name    = "dev"
+    public_key  = "dev.pub"
+    user        = "centos"
+  }
+}
+
+variable "chef_server" {
+  default = {
+    key = "/Users/amritgill/chef-repo/.chef/amgill.pem"
+  }
+}
