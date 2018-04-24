e3af148cb385c95644d3ef5c100c6880cc036015
Amrit Gill <amgill1234@gmail.com>
2018-04-23 21:26:16 -0400

    remove sh script

--- Kubernetes/artifactory/just_run_my_commands.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $HOME/infra-as-code/Kubernetes/artifactory
-kubectl apply -f artifactory-deployment.yml
-kubectl expose deployment artifactory-deployment --type=NodePort
-
-echo "***********************************"
-echo "Access artifactory server at: $(minikube service artifactory-deployment --url) "
