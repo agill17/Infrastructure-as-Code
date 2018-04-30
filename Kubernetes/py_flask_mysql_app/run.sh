d77a1cf7c2dd92b202dc8d4e18e926947b3c1f19
agill17 <amgill1234@gmail.com>
2018-04-30 00:12:03 -0400

    fixed shell

--- Kubernetes/py_flask_mysql_app/run.sh
+++ Kubernetes/py_flask_mysql_app/run.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-cd deployment
+deploy() {
 
-kubectl apply -f db_deployment.yml
-kubectl apply -f db_svc.yml
+	kubectl apply -f deployment/
 
-kubectl apply -f app_deployment.yml
-kubectl apply -f app_svc.yml
+}
+
+clean() {
+	kubectl delete deployment $(kubectl get deployment --selector=$sel --output=jsonpath={.items..metadata.name})
+ 	kubectl delete svc $(kubectl get svc --selector=$sel --output=jsonpath={.items..metadata.name})
+ 	kubectl delete secrets $(kubectl get secrets --selector=$sel --output=jsonpath={.items..metadata.name})
+ 	kubectl delete configMap $(kubectl get configMap --selector=$sel --output=jsonpath={.items..metadata.name})
+
+}
+
+
+
+
+case $1 in 
+	deploy)
+		deploy
+		;;
+	clean)
+		clean
+		;;
+esac
+
+### should really just namespaces but for now its fine...
