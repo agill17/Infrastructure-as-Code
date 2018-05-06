7f3c01a5c0e58486dd0dd61f6984e118b4230f1f
Amrit Gill <amgill1234@gmail.com>
2018-05-05 21:05:15 -0400

    fix shared library for jenkins

--- Jenkinsfile/sharedLibs/src/com/amrit/jenkinsLib/Utils.groovy
+++ Jenkinsfile/sharedLibs/src/com/amrit/jenkinsLib/Utils.groovy
@@ -5,24 +5,23 @@ import jdk.nashorn.internal.parser.JSONParser
 
 
 class Utils implements Serializable {
+  
+    def script
 
-    def env
-    def steps
-
-    Utils(env, steps){
-        this.env = env
-        this.steps = steps
+  
+    Utils(script){
+        this.script = script
     }
 
     // Most of these methods will really work on multi-branch jobs
 
     def getCommitId(){
-        commitId = steps.sh returnStdout: true, script: 'git rev-parse --short HEAD'
+        commitId = script.sh returnStdout: true, script: 'git rev-parse --short HEAD'
         return commitId
     }
 
     def getGitRepoName() {
-        def repoName = steps.sh(returnStdout: true, script: "git config --get remote.origin.url").split('/')[-1].replaceAll('.git', '')
+        def repoName = script.sh(returnStdout: true, script: "git config --get remote.origin.url").split('/')[-1].replaceAll('.git', '')
         return repoName
     }
 
@@ -37,7 +36,6 @@ class Utils implements Serializable {
         return [gitRepo: getGitRepoName(), branch: env.BRANCH_NAME, branchType: getBranchType()]
     }
 
-    @NonCPS
     def parseJSONFile(Map config){
         if (new File(config.file).exists()) {
             def parser = new JsonSlurper().parse(config.file)
@@ -58,7 +56,7 @@ class Utils implements Serializable {
     def getValueFromPropertyFile(Map config){
         if (!config.file && !config.lookUpValue){
             if (new File(config.file).exists()){
-                def parser = steps.readProperties file: config.file
+                def parser = script.readProperties file: config.file
                 return parser.${config.lookUpValue}
             } else{
                 println("No such file!! " + config.file)
@@ -89,7 +87,7 @@ class Utils implements Serializable {
     }
 
     def mvnBuildCommands(def cmds=["mvn package"], String imgName="maven:latest"){
-        docker.image(imgName).inside {
+        script.docker.image(imgName.inside {
             cmds.each { x ->
                 sh x
             }
@@ -97,7 +95,7 @@ class Utils implements Serializable {
     }
 
     def buildDockerImage(String imgName, String args){
-        image = docker.build(imgName, args)
+        image = script.docker.build(imgName, args)
         return image
     }
 
@@ -116,11 +114,11 @@ class Utils implements Serializable {
         def useImageObj = (imgObj != null || !imgObj.empty) ? true : false
 
         if (useImageObj){
-            docker.withRegistry(registry, credentialId){
+            script.docker.withRegistry(registry, credentialId){
                 imgObj.push()
             }
         } else if (!whichImage.empty && whichImage != null && isImageAvailLocally(whichImage) == true) {
-            sh(returnStdout: true, script: "docker push whichImage")
+            script.sh(returnStdout: true, script: "docker push whichImage")
         } else {
             println "You must pass in Image name as first parameter or the image object as the last parameter"
         }
@@ -128,7 +126,7 @@ class Utils implements Serializable {
 
     def runDockerContainer(String imgName, String args){
         if (!imgName.empty) {
-            container = docker.image(imgName).run(args)
+            container = steps.docker.image(imgName).run(args)
             return container
         }
     }
@@ -154,17 +152,17 @@ class Utils implements Serializable {
 
     def stopDockerContainerByName(String containerName){
         isRunning = containerRunning(containerName)
-        sh(returnStdout: true, script: "docker stop ${containerName}").trim()
+        script.sh(returnStdout: true, script: "docker stop ${containerName}").trim()
 
     }
 
     def rmDockerContainerByName(String containerName){
         isRunning = containerRunning(containerName)
-        sh(returnStdout: true, script: "docker stop ${containerName}").trim()
+        script.sh(returnStdout: true, script: "docker stop ${containerName}").trim()
     }
 
     def rmAllContainers(){
-        zombiePlusRunning = sh(returnStdout: true, script: 'docker ps -aq').trim()
+        zombiePlusRunning = script.sh(returnStdout: true, script: 'docker ps -aq').trim()
         if (!zombiePlusRunning.empty()){
             println "Stopping and removing these containers --> $zombiePlusRunning"
             zombiePlusRunning.tokenize().each() { c ->
--- Jenkinsfile/sharedLibs/vars/buildAndPushDockerImage.groovy
+++ Jenkinsfile/sharedLibs/vars/buildAndPushDockerImage.groovy
@@ -2,8 +2,12 @@ import com.amrit.jenkinsLib.Constants
 import com.amrit.jenkinsLib.Utils
 
 def call(Map config) {
+    /*
+        When calling methods from src/ make sure to pass this as constructor value
+        It enables src methods to utilize basically all steps from pipeline that are built in
+    */
 
-    def utils = new Utils()
+    def utils = new Utils(this)
 
     def imgName = config.imgName ?: 'agill17/imagefromjenkinsfile'
     def buildArgs = config.buildArgs
