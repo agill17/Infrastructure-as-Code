69deac6e44371b24bbbc3dbcf62476a61bf4e9a1
agill17 <amrit.gill@coveros.com>
2018-10-13 17:56:45 -0400

    statefulset sample

---
+++ Kubernetes/statefulset-sample/zookeeper/config.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+### to find other peers in statefulset, you have to use OtherPodName-INDEX.GoverningSVC 
+
+CURRENT_ID=$(hostname -s | cut -d "-" -f 2)
+CURRENT_ID=$((CURRENT_ID + 1 ))
+GOVERNING_SVC=$(hostname -d)
+HOSTNAME=$(hostname -s | cut -d "-" -f 1)
+
+echo "${ZOOKEEPER_DATA_DIR}/myid contents---------"
+echo $CURRENT_ID > $ZOOKEEPER_DATA_DIR/myid
+cat $ZOOKEEPER_DATA_DIR/myid
+echo "${ZOOKEEPER_DATA_DIR}/myid contents---------"
+echo ""
+if [[ $ZOOKEEPER_REPLICAS -gt 1 ]]; then
+  for ((i = 0; i < $ZOOKEEPER_REPLICAS; i++)); do
+    echo "server.$(( i + 1))=${HOSTNAME}-${i}.${GOVERNING_SVC}:2888:3888" >> $ZOOKEEPER_CFG
+  done
+fi
+echo "${ZOOKEEPER_CFG} contents---------"
+cat $ZOOKEEPER_CFG
+echo "${ZOOKEEPER_CFG} contents---------"
+
+
+${ZOOKEEPER_HOME}/bin/zkServer.sh start-foreground
