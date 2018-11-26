afedf20301c451a4537c837b6ed5c85da799e075
agill17 <amrit.gill@coveros.com>
2018-11-25 23:52:00 -0500

    add configuration for adding topics

--- Kubernetes/statefulset-sample/kafka/setup.sh
+++ Kubernetes/statefulset-sample/kafka/setup.sh
@@ -1,115 +1,24 @@
 #!/usr/bin/env bash
-
 POD_INDEX=$(hostname -s | cut -d "-" -f 2)
 BROKER_ID=$((POD_INDEX + 1))
 
 cat <<EOF >> $KAFKA_CONF_FILE
-############################ Server Basics #############################
-
-# The id of the broker. This must be set to a unique integer for each broker.
 broker.id=${BROKER_ID}
-
-############################# Socket Server Settings #############################
-
-# The port the socket server listens on
-port=9092
-
-# Hostname the broker will bind to. If not set, the server will bind to all interfaces
-#host.name=localhost
-
-# Hostname the broker will advertise to producers and consumers. If not set, it uses the
-# value for "host.name" if configured.  Otherwise, it will use the value returned from
-# java.net.InetAddress.getCanonicalHostName().
-#advertised.host.name=<hostname routable by clients>
-advertised.listeners=PLAINTEXT://${POD_IP}:9092
-
-# The port to publish to ZooKeeper for clients to use. If this is not set,
-# it will publish the same port that the broker binds to.
-#advertised.port=<port accessible by clients>
-
-# The number of threads handling network requests
-num.network.threads=2
- 
-# The number of threads doing disk I/O
+num.network.threads=3
 num.io.threads=8
-
-# The send buffer (SO_SNDBUF) used by the socket server
-socket.send.buffer.bytes=1048576
-
-# The receive buffer (SO_RCVBUF) used by the socket server
-socket.receive.buffer.bytes=1048576
-
-# The maximum size of a request that the socket server will accept (protection against OOM)
+advertised.listeners=PLAINTEXT://${POD_IP}:9092
+socket.send.buffer.bytes=102400
+socket.receive.buffer.bytes=102400
 socket.request.max.bytes=104857600
-
-
-############################# Log Basics #############################
-
-# A comma seperated list of directories under which to store log files
-log.dirs=${DEFAULT_LOGS_DIR:-/var/logs/kafka}
-
-# The default number of log partitions per topic. More partitions allow greater
-# parallelism for consumption, but this will also result in more files across
-# the brokers.
+log.dirs=${LOG_DIR:-/var/logs/kafka}
 num.partitions=${DEFAULT_PARTITIONS:-1}
-
-############################# Log Flush Policy #############################
-
-# Messages are immediately written to the filesystem but by default we only fsync() to sync
-# the OS cache lazily. The following configurations control the flush of data to disk. 
-# There are a few important trade-offs here:
-#    1. Durability: Unflushed data may be lost if you are not using replication.
-#    2. Latency: Very large flush intervals may lead to latency spikes when the flush does occur as there will be a lot of data to flush.
-#    3. Throughput: The flush is generally the most expensive operation, and a small flush interval may lead to exceessive seeks. 
-# The settings below allow one to configure the flush policy to flush data after a period of time or
-# every N messages (or both). This can be done globally and overridden on a per-topic basis.
-
-# The number of messages to accept before forcing a flush of data to disk
-#log.flush.interval.messages=10000
-
-# The maximum amount of time a message can sit in a log before we force a flush
-#log.flush.interval.ms=1000
-
-############################# Log Retention Policy #############################
-
-# The following configurations control the disposal of log segments. The policy can
-# be set to delete segments after a period of time, or after a given size has accumulated.
-# A segment will be deleted whenever *either* of these criteria are met. Deletion always happens
-# from the end of the log.
-
-# The minimum age of a log file to be eligible for deletion
-log.retention.hours=${DEFAULT_LOG_RETENTION_HOURS:-168}
-
-# A size-based retention policy for logs. Segments are pruned from the log as long as the remaining
-# segments don't drop below log.retention.bytes.
-#log.retention.bytes=1073741824
-
-# The maximum size of a log segment file. When this size is reached a new log segment will be created.
-log.segment.bytes=536870912
-
-# The interval at which log segments are checked to see if they can be deleted according 
-# to the retention policies
-log.retention.check.interval.ms=60000
-
-# By default the log cleaner is disabled and the log retention policy will default to just delete segments after their retention expires.
-# If log.cleaner.enable=true is set the cleaner will be enabled and individual logs can then be marked for log compaction.
-log.cleaner.enable=false
-delete.topic.enable=${DEFAULT_DELETE_TOPIC_POLICY:-true}
-auto.create.topics.enable=${ENABLE_AUTOCREATE_TOPICS:-true}
-############################# Zookeeper #############################
-
-# Zookeeper connection string (see zookeeper docs for details).
-# This is a comma separated host:port pairs, each corresponding to a zk
-# server. e.g. "127.0.0.1:3000,127.0.0.1:3001,127.0.0.1:3002".
-# You can also append an optional chroot string to the urls to specify the
-# root directory for all kafka znodes.
+num.recovery.threads.per.data.dir=1
+log.retention.hours=${RETENTION_PERIOD:-12}
+log.segment.bytes=1073741824
+log.retention.check.interval.ms=300000
+delete.topic.enable=true
+zookeeper.connection.timeout.ms=6000
 zookeeper.connect=${ZOOKEEPER}
-
-# Timeout in ms for connecting to zookeeper
-zookeeper.connection.timeout.ms=1000000
 EOF
-
-
 cat $KAFKA_CONF_FILE
-
 ./bin/kafka-server-start.sh $KAFKA_CONF_FILE
