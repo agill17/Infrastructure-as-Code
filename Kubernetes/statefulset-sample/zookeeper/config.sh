c2be13b10cc640c5d927f4c9b20f2faca91e290c
agill17 <amrit.gill@coveros.com>
2018-12-03 00:04:02 -0500

    init helm chart for kafka and zoo

--- Kubernetes/statefulset-sample/zookeeper/config.sh
+++ Kubernetes/statefulset-sample/zookeeper/config.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
 ### to find other peers in statefulset, you have to use OtherPodName-INDEX.GoverningSVC 
-
+set -x
 CURRENT_ID=$(hostname -s | cut -d "-" -f 2)
 CURRENT_ID=$((CURRENT_ID + 1 ))
 GOVERNING_SVC=$(hostname -d)
 HOSTNAME=$(hostname -s | cut -d "-" -f 1)
+DATA_DIR=${ZK_DATA_DIR:-/data/zookeeper}
 touch $ZOOKEEPER_CFG
 echo "${ZOOKEEPER_DATA_DIR}/myid contents---------"
-echo $CURRENT_ID > $ZOOKEEPER_DATA_DIR/myid
-cat $ZOOKEEPER_DATA_DIR/myid
-echo "${ZOOKEEPER_DATA_DIR}/myid contents---------"
+echo $CURRENT_ID > $DATA_DIR/myid
+cat $DATA_DIR/myid
+echo "${DATA_DIR}/myid contents---------"
 echo ""
 if [[ $ZOOKEEPER_REPLICAS -gt 1 ]]; then
   for ((i = 0; i < $ZOOKEEPER_REPLICAS; i++)); do
@@ -20,7 +21,7 @@ fi
 
 cat << EOF >> $ZOOKEEPER_CFG 
 tickTime=2000
-dataDir=${DATA_DIR:-/data/zookeeper}
+dataDir=${DATA_DIR}
 clientPort=2181
 initLimit=5
 syncLimit=2
