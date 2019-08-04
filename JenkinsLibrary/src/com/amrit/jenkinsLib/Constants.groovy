0790c38e9bdd7145ca16ec6c791c74d92f14b0ea
agill17 <amrit.gill@coveros.com>
2019-08-04 16:36:33 -0400

    add sample typed jenkins library

---
+++ JenkinsLibrary/src/com/amrit/jenkinsLib/Constants.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+package com.amrit.jenkinsLib
+
+class Constants implements Serializable {
+  static final NEXUS_URL = ""
+  static final NEXUS_REPO = ""
+  static final SONAR_URL = ""
+  static final SONAR_CRED_ID = ""
+  static final NEXUS_CRED_ID = ""
+  static final GIT_URL = ""
+  static final DOCKER_PUBLIC_REGISTRY = "https://registry.hub.docker.com"
+  static final DOCKER_CRED_ID = ""
+
+}
